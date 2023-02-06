e311754058da624669add70d89b5df98254b4fa1
Gustavo Sosa <gustavososa92@gmail.com>
2023-02-05 21:05:13 -0300

    add vars

--- holaMundo.go
+++ holaMundo.go
@@ -1,8 +1,17 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
 func main() {
+	var suma int = 8 + 9
+	var texto string = "El resultado es:"
+	otroTexto := "LALALA"
 
 	fmt.Println("Hola Mundo")
+	fmt.Println(otroTexto)
+	time.Sleep(time.Second * 1)
+	fmt.Println(texto, suma)
 }
