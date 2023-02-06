382e88f311881f66b34c42d115557e31bd4ab93e
Gustavo Sosa <gustavososa92@gmail.com>
2023-02-05 22:21:30 -0300

    add args and strconv

--- main1.go
+++ main1.go
@@ -3,6 +3,8 @@ package main
 import (
 	"errors"
 	"fmt"
+	"os"
+	"strconv"
 )
 
 type Gorra struct {
@@ -24,6 +26,31 @@ func main() {
 		plana:  false,
 	}
 
+	var peliculas [3]string
+
+	peliculas[0] = "1p"
+	peliculas[1] = "2p"
+	peliculas[2] = "3p"
+
+	otraPeli := [3]string{
+		"asda",
+		"werwer",
+		"iuyoiuyo",
+	}
+
+	var comentarios [3][2]string
+
+	comentarios[0][0] = "0asd"
+	comentarios[0][1] = "1asd"
+	comentarios[1][0] = "2asd"
+	comentarios[1][1] = "3asd"
+	comentarios[2][0] = "4asd"
+	comentarios[2][1] = "5asd"
+
+	var pelis []string
+	pelis = append(pelis, "ASD")
+	pelis2 := append(pelis, "123")
+
 	fmt.Println("Hola Mundo")
 	fmt.Println(otroTexto)
 	fmt.Println(texto, suma)
@@ -35,6 +62,24 @@ func main() {
 	fmt.Println(operacion(10, 5, "/"))
 	fmt.Println(operacion(10, 5, "/+"))
 	fmt.Println(gorras(45, "$"))
+	pantalon("rojo", "corto", "saraza")
+	fmt.Println(peliculas)
+	fmt.Println(peliculas[1])
+	fmt.Println(otraPeli)
+	fmt.Println(otraPeli[1])
+	fmt.Println(comentarios)
+	fmt.Println(pelis)
+	fmt.Println(pelis2)
+	fmt.Println((os.Args))
+	valor, _ := strconv.Atoi(os.Args[2])
+	fmt.Println(valor + 1)
+
+}
+
+func pantalon(caracteristicas ...string) {
+	for _, c := range caracteristicas {
+		fmt.Println(c)
+	}
 }
 
 func gorras(pedido float32, moneda string) (string, string, float32) {
