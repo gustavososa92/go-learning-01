a10ac9178f254e9e47c274cb917ab3db51b1c6db
Gustavo Sosa <gustavososa92@gmail.com>
2023-02-12 18:16:17 -0300

    add post to mongo

--- main/actions.go
+++ main/actions.go
@@ -1,12 +1,37 @@
 package main
 
 import (
+	"context"
 	"encoding/json"
 	"fmt"
 	"github.com/gorilla/mux"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 	"log"
 	"net/http"
 )
+
+func getSession() *mongo.Client {
+
+	credential := options.Credential{
+		AuthSource: "moviesGO",
+		Username: "admin",
+		Password: "password",
+	 }
+
+	 clientOpts := options.Client().ApplyURI("mongodb://localhost:27017").SetAuth(credential)
+
+	 client, err := mongo.Connect(context.TODO(), clientOpts)
+
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Successfully connected.")
+	return client
+}
+
+var moviesCollection = getSession().Database("moviesGO").Collection("movies")
 
 var movies = Movies{
 	Movie{"PELI 1", 2020, "Yo1"},
@@ -32,15 +57,24 @@ func MovieById(w http.ResponseWriter, r *http.Request) {
 
 func MovieAdd(w http.ResponseWriter, r *http.Request) {
 	decoder := json.NewDecoder(r.Body)
-	
+
 	var movieData Movie
-	err:=decoder.Decode(&movieData)
+	err := decoder.Decode(&movieData)
 	if err != nil {
 		panic(err)
 	}
 	defer r.Body.Close()
-	
+
 	log.Println(movieData)
 
-	movies = append(movies, movieData)
+	_, err = moviesCollection.InsertOne(context.TODO(), movieData)
+	if err != nil {
+		log.Println(err)
+		w.WriteHeader(500)
+		return
+	}
+
+	json.NewEncoder(w).Encode(movieData)
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(200)
 }
